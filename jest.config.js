5bfcde2de2061dcc1eedb6f4ac625eb8b6ea8c95
leandrofuccia <leandro.fuccia@gmail.com>
2025-03-02 19:00:46 -0300

    require('dotenv').config()

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,10 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    testMatch: ['**/?(*.)+(spec|test).ts?(x)'],
-    moduleNameMapper: {
-      '^@/(.*)$': '<rootDir>/src/$1',
-    },
-  };
+// eslint-disable-next-line no-undef
+require('dotenv').config(); // Carrega as variáveis de ambiente do arquivo .env
 
-
+export const preset = 'ts-jest';
+export const testEnvironment = 'node';
+export const moduleFileExtensions = ['ts', 'tsx', 'js', 'jsx', 'json', 'node'];
+export const testMatch = ['**/?(*.)+(spec|test).ts?(x)'];
+export const moduleNameMapper = {
+  '^@/(.*)$': '<rootDir>/src/$1',
+};
