1e3225b4c47c9412ad685b1d2667e3e23c1b9c6e
Andreia Silva <apsilva3@gmail.com>
2025-03-04 15:19:25 -0300

    Inicializando banco

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,22 @@
 import { app } from './app';
+import { initializeDatabase } from './lib/typeorm/typeorm'; 
 
-app.listen({ port: 3002, host: '0.0.0.0' }, (err, address) => {
-  if (err) {
-    console.error(err);
+async function startServer() {
+  try {
+    await initializeDatabase();
+    console.log('Database initialized.');
+
+    app.listen({ port: 3002, host: '0.0.0.0' }, (err, address) => {
+      if (err) {
+        console.error('Error starting server:', err);
+        process.exit(1);
+      }
+      console.log(`Server is running on ${address}`);
+    });
+  } catch (error) {
+    console.error('Error initializing the database:', error);
     process.exit(1);
   }
-  console.log(`Server is running on ${address}`);
-});
+}
+
+startServer();
